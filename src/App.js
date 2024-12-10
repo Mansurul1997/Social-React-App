4b241c7d9d93964dcc80fcff7484e4bb3beae429
Md. Mansurul Haque <71166929+Mansurul1997@users.noreply.github.com>
2024-12-10 16:42:38 +0600

    socialAppCode

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Home from "./pages/home/home";
+import {Person} from "@material-ui/icons"
+
+function App() {
+  return(
+    <>
+    <div>
+
+    <h1>
+    <Person />
+
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App;
